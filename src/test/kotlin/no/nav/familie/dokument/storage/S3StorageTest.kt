67cae953d0f3a935ae54e806eb563ff5427fce7d
Martine Enger <marenger@student.matnat.uio.no>
2019-11-21 10:00:50 +0100

    [kaizen] Tar i bruk mockk

---
+++ src/test/kotlin/no/nav/familie/dokument/storage/S3StorageTest.kt
@@ -0,0 +1,16 @@
+package no.nav.familie.dokument.storage
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.dokument.storage.s3.S3Storage
+import org.junit.Test
+import java.util.*
+
+class S3StorageTest {
+
+    @Test
+    fun testMockk() {
+        val storage: S3Storage = mockk()
+        //every { storage[any(), any()] } returns Optional.of("filinnhold".toByteArray())
+    }
+}
--- src/test/kotlin/no/nav/familie/dokument/storage/StorageDevConfiguration.kt
+++ src/test/kotlin/no/nav/familie/dokument/storage/StorageDevConfiguration.kt
@@ -1,13 +1,15 @@
 package no.nav.familie.dokument.storage
 
+import io.mockk.every
+import io.mockk.mockk
 import no.nav.familie.dokument.storage.s3.S3Storage
-import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
 import java.util.*
+
 
 @Profile("dev")
 @Configuration
@@ -16,11 +18,8 @@ class StorageDevConfiguration {
     @Bean
     @Primary
     fun s3Storage(): S3Storage {
-        val storage = Mockito.mock(S3Storage::class.java)
-
-        // TODO: Finn en mock-løsning som funker med Kotlin
-        // Mockito.`when`(storage[any(), any()]).thenReturn(Optional.of("filinnhold".toByteArray()))
-
+        val storage = mockk<S3Storage>()
+        every { storage[any(), any()] } returns Optional.of("filinnhold".toByteArray())
         return storage
     }
 }
