174675e085609b9abecd08e4993add75b5f4d857
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-06-04 14:15:19 +0200

    Lagt inn config for å kunne kjøre mest mulig fornuftig lokalt

--- src/test/kotlin/no/nav/familie/dokument/config/TestStorageConfiguration.kt
+++ src/test/kotlin/no/nav/familie/dokument/config/TestStorageConfiguration.kt
@@ -1,9 +1,6 @@
 package no.nav.familie.dokument.config
 
-import io.mockk.Runs
-import io.mockk.every
-import io.mockk.just
-import io.mockk.mockk
+import io.mockk.*
 import no.nav.familie.dokument.storage.attachment.AttachmentStorage
 import no.nav.familie.dokument.storage.attachment.AttachmentToStorableFormatConverter
 import no.nav.familie.dokument.storage.attachment.ImageConversionService
@@ -11,7 +8,9 @@ import no.nav.familie.dokument.storage.encryption.EncryptedStorage
 import no.nav.familie.dokument.storage.s3.S3Storage
 import no.nav.security.token.support.test.spring.TokenGeneratorConfiguration
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.*
+import java.io.InputStream
 
 
 @Profile("dev")
@@ -19,14 +18,22 @@ import org.springframework.context.annotation.*
 @Import(TokenGeneratorConfiguration::class)
 class TestStorageConfiguration {
 
+    val lokalStorage: MutableMap<String, ByteArray> = HashMap()
+
     @Bean
     @Primary
     fun encryptedStorage(): EncryptedStorage {
         val storage: EncryptedStorage = mockk()
 
-
-        every { storage[any(), any()] } returns "filinnhold".toByteArray()
-
+        val slot = slot<String>()
+        val slotPut = slot<String>()
+        val slotInputStream = slot<InputStream>()
+        every { storage[capture(slot), any()] } answers {
+            lokalStorage.getOrElse(slot.captured, { error("ingenting her") })
+        }
+        every { storage.put(capture(slotPut), any(), capture(slotInputStream)) } answers {
+            lokalStorage[slotPut.captured] = slotInputStream.captured.readAllBytes()
+        }
         return storage
     }
 
@@ -53,9 +60,18 @@ class TestStorageConfiguration {
             storableFormatConverter: AttachmentToStorableFormatConverter): AttachmentStorage {
         val storage: AttachmentStorage = mockk()
 
-        every { storage.put(any(), any(),any()) }  just Runs
+        every { storage.put(any(), any(), any()) } just Runs
         every { storage[any(), any()] } returns "filinnhold".toByteArray()
 
         return storage
     }
+
+    @Bean
+    fun corsFilter(corsProperties: CorsProperties): FilterRegistrationBean<CORSResponseFilter> {
+        val filterRegistration = FilterRegistrationBean<CORSResponseFilter>()
+        filterRegistration.filter = CORSResponseFilter(corsProperties)
+        filterRegistration.order = 0
+        return filterRegistration
+    }
+
 }
