4b1ef8689aad7a2bb77ce8bd9b807fd48a114ec6
Johan Blomgren <johan.blomgren@nav.no>
2020-10-22 14:59:14 +0200

    Fikset config

--- src/main/kotlin/no/nav/familie/dokument/virusscan/VirusScanConfig.kt
+++ src/main/kotlin/no/nav/familie/dokument/virusscan/VirusScanConfig.kt
@@ -1,16 +1,8 @@
 package no.nav.familie.dokument.virusscan
 
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
-import org.springframework.boot.context.properties.bind.DefaultValue
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
 import java.net.URI
 
-@ConfigurationProperties(prefix = "virus")
-@ConstructorBinding
-class VirusScanConfig constructor(@DefaultValue(DEFAULT_CLAM_URI) val uri: URI) {
-
-    companion object {
-
-        private const val DEFAULT_CLAM_URI = "http://clamav.nais.svc.nais.local/scan"
-    }
-}
+@Configuration
+class VirusScanConfig constructor(@Value("\${CLAM_AV_VIRUS_URL}") val uri: URI)
