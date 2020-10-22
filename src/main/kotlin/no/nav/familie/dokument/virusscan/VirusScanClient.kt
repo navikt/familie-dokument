021d06ec2e34ed555ebea07a5bcfac3551948a70
Johan Blomgren <johan.blomgren@nav.no>
2020-10-22 19:35:40 +0200

    Prøver annen path for healthcheck

--- src/main/kotlin/no/nav/familie/dokument/virusscan/VirusScanClient.kt
+++ src/main/kotlin/no/nav/familie/dokument/virusscan/VirusScanClient.kt
@@ -3,21 +3,24 @@ package no.nav.familie.dokument.virusscan
 import no.nav.familie.http.client.AbstractPingableRestClient
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestOperations
+import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
 
 @Service
 class VirusScanClient(operations: RestOperations, private val config: VirusScanConfig) :
         AbstractPingableRestClient(operations, "virusscan") {
 
+    private val scanUri = UriComponentsBuilder.fromUri(config.uri).path("scan").build().toUri()
+
     fun scan(bytes: ByteArray): List<ScanResult> {
         try {
-            return putForEntity(config.uri, bytes)
+            return putForEntity(scanUri, bytes)
         } catch (e: Exception) {
             throw VirusScanException("Feilet virusscanning", e)
         }
     }
 
     override val pingUri: URI
-        get() = config.uri
+        get() = UriComponentsBuilder.fromUri(config.uri).path("liveness").build().toUri()
 
 }
