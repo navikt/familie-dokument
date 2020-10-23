85677984d4598b7edf29a18839978dc8a5d27791
Meng Zhu <meng.zhu@nav.no>
2020-10-23 14:11:42 +0200

    update test code

--- src/main/kotlin/no/nav/familie/dokument/DebugController.kt
+++ src/main/kotlin/no/nav/familie/dokument/DebugController.kt
@@ -1,24 +1,30 @@
 package no.nav.familie.dokument
 
 import no.nav.familie.dokument.storage.attachment.AttachmentStorage
+import no.nav.familie.dokument.storage.google.GcpStorageConfiguration.Companion.ATTACHMENT_GCP_STORAGE
+import no.nav.familie.dokument.storage.google.GcpStorageConfiguration.Companion.STONAD_GCP_STORAGE
+import no.nav.familie.dokument.storage.google.GcpStorageWrapper
 import no.nav.familie.dokument.storage.mellomlager.MellomLagerService
 import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Profile
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
+import java.io.ByteArrayInputStream
 
 @RestController
 @RequestMapping("debugger")
 @Profile("preprod")
 @Unprotected
-class DebugController(val soknadStorage: MellomLagerService, val attachmentStorage: AttachmentStorage) {
+class DebugController(@Qualifier(STONAD_GCP_STORAGE) val soknadGcpStorage: GcpStorageWrapper,
+@Qualifier(ATTACHMENT_GCP_STORAGE) val attachmentGcpStorage: GcpStorageWrapper) {
 
     @GetMapping("testSoknadStorage/{directory}/{filename}")
     fun testSoknadStorage(@PathVariable("directory") directory: String,
                           @PathVariable("filename") filename: String,
                           @RequestParam("content") content: String
     ): String{
-        soknadStorage.put(directory, filename, content)
-        return soknadStorage[directory, filename]
+        soknadGcpStorage.put(directory, filename, ByteArrayInputStream(content.toByteArray()))
+        return String(soknadGcpStorage[directory, filename])
     }
 }
