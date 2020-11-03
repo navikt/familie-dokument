a7380d3776fec2b5e767d3c93cf600571767d8f8
Johan Blomgren <johan.blomgren@nav.no>
2020-11-03 14:09:54 +0100

    Småfiks

--- src/main/kotlin/no/nav/familie/dokument/storage/google/GcpStorage.kt
+++ src/main/kotlin/no/nav/familie/dokument/storage/google/GcpStorage.kt
@@ -1,13 +1,14 @@
 package no.nav.familie.dokument.storage.google
 
-import com.google.cloud.storage.*
+import com.google.cloud.storage.BlobId
+import com.google.cloud.storage.BlobInfo
+import com.google.cloud.storage.Storage
 import no.nav.familie.dokument.GcpDocumentNotFound
 import org.apache.commons.io.IOUtils
-import org.apache.http.HttpStatus
 import org.slf4j.LoggerFactory
 import java.io.InputStream
 
-class GcpStorage(val bucketName: String, maxFileSizeMB: Int, val storage: Storage) {
+class GcpStorage(private val bucketName: String, maxFileSizeMB: Int, private val storage: Storage) {
 
     private val maxFileSizeAfterEncryption: Int = (maxFileSizeMB.toDouble() * 1000.0 * 1000.0 * ENCRYPTION_SIZE_FACTOR).toInt()
 
@@ -16,9 +17,9 @@ class GcpStorage(val bucketName: String, maxFileSizeMB: Int, val storage: Storag
     fun put(directory: String, key: String, data: InputStream, mediaTypeValue: String) {
         try {
             val bytes = IOUtils.toByteArray(data)
-            LOG.debug("Bufret stream som gav antall bytes: " + bytes.size)
+            LOG.debug("Bufret stream som gav antall bytes: ${bytes.size}")
             if(bytes.size > maxFileSizeAfterEncryption){
-                throw RuntimeException("GcpStorage feil: vedlegg overskrider filstørrelsesgrensen\n")
+                throw RuntimeException("GcpStorage feil: vedlegg overskrider filstørrelsesgrensen")
             }
 
             val blobInfo = BlobInfo.newBuilder(BlobId.of(bucketName, makeKey(directory, key)))
@@ -36,12 +37,12 @@ class GcpStorage(val bucketName: String, maxFileSizeMB: Int, val storage: Storag
 
     fun delete(directory: String, key: String) {
         storage.delete(BlobId.of(bucketName, makeKey(directory, key)))
-        LOG.debug("Deleted file from bucket ${key}")
+        LOG.debug("Deleted file from bucket $key")
     }
 
     companion object {
 
         private val LOG = LoggerFactory.getLogger(GcpStorage::class.java)
-        private val ENCRYPTION_SIZE_FACTOR = 1.5
+        private const val ENCRYPTION_SIZE_FACTOR = 1.5
     }
 }
