94e9592eb2aa0de9488c5868a61c8b57de645915
mundal <halvorbmundal@gmail.com>
2020-11-19 09:49:17 +0100

    Gjør om output til byte array

--- src/main/kotlin/no/nav/familie/dokument/pdf/PdfController.kt
+++ src/main/kotlin/no/nav/familie/dokument/pdf/PdfController.kt
@@ -2,8 +2,6 @@ package no.nav.familie.dokument.storage
 
 import no.nav.familie.dokument.pdf.PdfService
 import no.nav.security.token.support.core.api.Unprotected
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,11 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 class PdfController(val pdfService: PdfService) {
 
     @PostMapping
-    fun lagPdf(@RequestBody html: String): ResponseEntity<*> {
-        return ResponseEntity(
-            pdfService.lagPdf(html),
-            pdfService.lagPdfHeadere("dokument"),
-            HttpStatus.OK
-        )
+    fun lagPdf(@RequestBody html: String): ByteArray {
+        return pdfService.lagPdf(html)
     }
 }
