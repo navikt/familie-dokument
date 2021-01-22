58e788ad89633759f73dfe569abd8ab51478ef2c
Henning Håkonsen <henning.hakonsen@nav.no>
2021-01-22 12:38:33 +0100

    Legger til familie-brev dev i allowed cors.

--- src/main/kotlin/no/nav/familie/dokument/pdf/PdfController.kt
+++ src/main/kotlin/no/nav/familie/dokument/pdf/PdfController.kt
@@ -1,12 +1,10 @@
-package no.nav.familie.dokument.storage
+package no.nav.familie.dokument.pdf
 
-import no.nav.familie.dokument.pdf.PdfService
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-
 
 @RestController
 @RequestMapping("api")
