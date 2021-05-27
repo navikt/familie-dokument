918a6147d8b6d13f16a478c330e0f0932fd1cd3d
Meng Zhu <meng.zhu@nav.no>
2021-05-27 05:17:12 +0200

    add endpoint

--- src/main/kotlin/no/nav/familie/dokument/pdf/PdfController.kt
+++ src/main/kotlin/no/nav/familie/dokument/pdf/PdfController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("familie/dokument/api", "api")
 @Unprotected
 class PdfController(val pdfService: PdfService) {
 
