fb3c98e424c9132abe01147e6e61190de97f918a
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2026-05-26 15:08:34 +0200

    Flytt CORS-konfigurasjon fra CORSResponseFilter til SecurityConfig

--- src/main/kotlin/no/nav/familie/dokument/config/SecurityConfig.kt
+++ src/main/kotlin/no/nav/familie/dokument/config/SecurityConfig.kt
@@ -1,21 +1,29 @@
 package no.nav.familie.dokument.config
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+@EnableConfigurationProperties(CorsProperties::class)
+class SecurityConfig(
+    private val corsProperties: CorsProperties,
+) {
     @Bean
     fun securityFilterChain(
         http: HttpSecurity,
         tokenXJwtDecoder: TokenXJwtDecoder,
     ): SecurityFilterChain {
         http {
+            cors { configurationSource = corsConfigurationSource() }
             csrf { disable() }
             authorizeHttpRequests {
                 authorize("/internal/**", permitAll)
@@ -30,5 +38,17 @@ class SecurityConfig {
             }
         }
         return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val corsConfiguration = CorsConfiguration()
+        corsConfiguration.allowedOrigins = corsProperties.allowedOrigins.toList()
+        corsConfiguration.allowedHeaders = listOf("origin", "content-type", "content-length", "accept", "authorization", "nav-consumer-id")
+        corsConfiguration.allowCredentials = true
+        corsConfiguration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+        return UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", corsConfiguration)
+        }
     }
 }
