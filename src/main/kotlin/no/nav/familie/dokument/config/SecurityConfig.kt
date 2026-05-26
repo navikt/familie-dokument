186ae3494470180925391b6a5d369823f72c6087
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2026-05-26 15:39:03 +0200

    Flytt feilhåndtering av 401 og 403 inn i SecurityConfig

--- src/main/kotlin/no/nav/familie/dokument/config/SecurityConfig.kt
+++ src/main/kotlin/no/nav/familie/dokument/config/SecurityConfig.kt
@@ -1,12 +1,22 @@
 package no.nav.familie.dokument.config
 
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import no.nav.familie.kontrakter.felles.Ressurs
+import no.nav.familie.kontrakter.felles.Ressurs.Companion.failure
+import no.nav.familie.kontrakter.felles.jsonMapper
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.access.AccessDeniedHandler
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
@@ -36,12 +46,15 @@ class SecurityConfig(
             oauth2ResourceServer {
                 jwt { jwtDecoder = tokenXJwtDecoder }
             }
+            exceptionHandling {
+                accessDeniedHandler = accessDeniedHandler()
+                authenticationEntryPoint = authenticationEntryPoint()
+            }
         }
         return http.build()
     }
 
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
+    private fun corsConfigurationSource(): CorsConfigurationSource {
         val corsConfiguration = CorsConfiguration()
         corsConfiguration.allowedOrigins = corsProperties.allowedOrigins.toList()
         corsConfiguration.allowedHeaders = listOf("origin", "content-type", "content-length", "accept", "authorization", "nav-consumer-id")
@@ -51,4 +64,39 @@ class SecurityConfig(
             registerCorsConfiguration("/**", corsConfiguration)
         }
     }
+
+    private fun accessDeniedHandler(): AccessDeniedHandler =
+        AccessDeniedHandler { _: HttpServletRequest, response: HttpServletResponse, _: AccessDeniedException ->
+            response.apply {
+                status = HttpServletResponse.SC_FORBIDDEN
+                contentType = MediaType.APPLICATION_JSON_VALUE
+                characterEncoding = Charsets.UTF_8.name()
+                jsonMapper.writeValue(
+                    writer,
+                    Ressurs(
+                        data = null,
+                        status = Ressurs.Status.IKKE_TILGANG,
+                        melding = "Bruker har ikke tilgang",
+                        frontendFeilmelding = "Du har ikke tilgang",
+                        stacktrace = null,
+                    ),
+                )
+            }
+        }
+
+    private fun authenticationEntryPoint(): AuthenticationEntryPoint =
+        AuthenticationEntryPoint { _: HttpServletRequest, response: HttpServletResponse, _: AuthenticationException ->
+            response.apply {
+                status = HttpServletResponse.SC_UNAUTHORIZED
+                contentType = MediaType.APPLICATION_JSON_VALUE
+                characterEncoding = Charsets.UTF_8.name()
+                jsonMapper.writeValue(
+                    writer,
+                    failure<Nothing>(
+                        errorMessage = "401 Unauthorized",
+                        frontendFeilmelding = "Kall ikke autorisert",
+                    ),
+                )
+            }
+        }
 }
