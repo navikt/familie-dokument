4d71a39aee3ebf77249f0c2cc69ee69ac2963f93
Stig Helge Strøm <stig.helge.strom@nav.no>
2026-06-25 12:54:50 +0200

    Legger tilk WWW-authenticate headerved unauthorized

--- src/main/kotlin/no/nav/familie/dokument/config/SecurityConfig.kt
+++ src/main/kotlin/no/nav/familie/dokument/config/SecurityConfig.kt
@@ -105,6 +105,7 @@ class SecurityConfig(
         AuthenticationEntryPoint { _: HttpServletRequest, response: HttpServletResponse, _: AuthenticationException ->
             response.apply {
                 status = HttpServletResponse.SC_UNAUTHORIZED
+                addHeader("WWW-Authenticate", "Bearer realm=\"familie-dokument\"")
                 contentType = MediaType.APPLICATION_JSON_VALUE
                 characterEncoding = Charsets.UTF_8.name()
                 jsonMapper.writeValue(
